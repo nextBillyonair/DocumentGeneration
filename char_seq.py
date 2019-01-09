73ef2792c12638e0ca6a91e483a32de68bc70d5e
nextBillyonair <wwatso13@jhu.edu>
2019-01-08 22:11:03 -0500

    Add generate function, plus resize datasets

--- char_seq.py
+++ char_seq.py
@@ -3,17 +3,25 @@ import random
 from torch.nn import Embedding, Linear, Module, LSTM, NLLLoss
 from torch.utils.data import DataLoader, Dataset
 from torch.optim import Adam
-from torch.nn.functional import relu, log_softmax
-
+from torch.nn.functional import relu, log_softmax, softmax
+from numpy.random import choice
+import numpy as np
 
 SEQ_LEN = 100
 BATCH_SIZE = 128
 HIDDEN_SIZE = 48
-DATASET = 'data_small.txt'
+DATASET = 'data_medium.txt'
 EPOCHS = 100
 LR = 1e-2
+GEN_LEN = 30
 
 torch.manual_seed(0)
+probability_distribution = np.array([0.08167, 0.01492, 0.02782, 0.04253, 0.12702,
+    0.02228, 0.02015, 0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406,
+    0.06749, 0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758,
+    0.00978, 0.02360, 0.00150, 0.01974, 0.00074])
+probability_distribution /= sum(probability_distribution)
+letters = list('abcdefghijklmnopqrstuvwxyz')
 
 
 class Model(Module):
@@ -30,6 +38,12 @@ class Model(Module):
         step = log_softmax(self.linear(step), dim=2)
         return step
 
+    def generate(self, last_token, hidden=None):
+        embed = self.character_embedding(last_token)
+        step, hidden = self.lstm(embed, hidden)
+        step = softmax(self.linear(step), dim=2)
+        return step, hidden
+
 
 class Characters():
 
@@ -45,6 +59,9 @@ class Characters():
             self._next_index += 1
 
         return self._char_indices[char]
+
+    def __getitem__(self, index):
+        return self._index_chars[index]
 
     def __len__(self):
         return len(self._char_indices)
@@ -117,10 +134,30 @@ class Trainer():
                     self.epoch, batch_idx * BATCH_SIZE, len(self.dataloader.dataset),
                     100. * batch_idx / len(self.dataloader), loss.item()))
 
+            if self.epoch % 10 == 0:
+                print(f'> {self.generate()}')
+
             print('====> Train set loss: {:.4f}'.format(self.total_loss / len(self.dataloader)))
+
+    def sample_letter(self):
+        draw = choice(letters, 1, p=probability_distribution)[0]
+        return self.dataset.characters.add_char(draw)
+
+    def generate(self):
+        last_token = torch.tensor([[self.sample_letter()]], dtype=torch.long)
+        sequence = [self.dataset.characters[last_token.item()]]
+        hidden = None
+        for _ in range(GEN_LEN):
+            output, hidden = self.model.generate(last_token, hidden)
+            _, last_token = output.topk(1)
+            last_token = last_token.squeeze(0)
+            sequence.append(self.dataset.characters[last_token.item()])
+        return ''.join(sequence)
+
 
 
 if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     t = Trainer(EPOCHS)
     t.train()
+    t.generate()
