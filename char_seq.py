9bdc062f87183b049e336e595501ac642cc58501
Achintya Gopal <achintyagopal@hotmail.com>
2019-01-08 21:03:00 -0500

    Refactor and batching

--- char_seq.py
+++ char_seq.py
@@ -1,22 +1,35 @@
 import torch
 import random
 from torch.nn import Embedding, Linear, Module, LSTM, NLLLoss
+from torch.utils.data import DataLoader, Dataset
 from torch.optim import Adam
 from torch.nn.functional import relu, log_softmax
 
-class Generator(Module):
+
+SEQ_LEN = 100
+BATCH_SIZE = 128
+HIDDEN_SIZE = 48
+DATASET = 'data_small.txt'
+EPOCHS = 100
+LR = 1e-2
+
+torch.manual_seed(0)
+
+
+class Model(Module):
 
     def __init__(self, out_size):
         super().__init__()
-        self.character_embedding = Embedding(out_size, 48)
-        self.lstm = LSTM(48, 48)
-        self.linear = Linear(48, out_size)
+        self.character_embedding = Embedding(out_size, HIDDEN_SIZE)
+        self.lstm = LSTM(HIDDEN_SIZE, HIDDEN_SIZE, batch_first=True)
+        self.linear = Linear(HIDDEN_SIZE, out_size)
 
-    def forward(self, input, hidden):
+    def forward(self, input):
         embed = self.character_embedding(input)
-        step, hidden = self.lstm(embed, hidden)
+        step, _ = self.lstm(embed)
         step = log_softmax(self.linear(step), dim=2)
-        return step, hidden
+        return step
+
 
 class Characters():
 
@@ -25,16 +38,7 @@ class Characters():
         self._index_chars = {}
         self._next_index = 0
 
-        self._add_char('<SOW>')
-        self._add_char('<EOW>')
-        self.add_word(list("abcdefghijklmnopqrstuvwxyz"))
-
-    def add_word(self, word):
-        indices = tuple(self._add_char(char) for char in list(word))
-        assert len(indices) > 0, 'empty sentence'
-        return indices
-
-    def _add_char(self, char):
+    def add_char(self, char):
         if char not in self._char_indices:
             self._char_indices[char] = self._next_index
             self._index_chars[self._next_index] = char
@@ -45,87 +49,78 @@ class Characters():
     def __len__(self):
         return len(self._char_indices)
 
-class Model():
 
-    def __init__(self, out_size):
-        self.model = Generator(out_size)
-
-    def parameters(self):
-        return self.model.parameters()
-
-    def train(self, word, optimizer, criterion):
-        optimizer.zero_grad()
-        loss = 0.0
-
-        last_token = torch.full((1,1), 0, dtype=torch.long)
-        hidden = None
-        for i in range(word.size(0)):
-            output, hidden = self.model(last_token, hidden)
-            _, most_likely_token_indices = output.topk(1)
-            last_token = most_likely_token_indices.squeeze(1).detach()
-            print(last_token, word[i].unsqueeze(0))
-
-            if random.random() < 1.0:
-                # print(last_token, word[i].unsqueeze(0).unsqueeze(0))
-                last_token = word[i].unsqueeze(0).unsqueeze(0)
-            loss += criterion(output.view(1, -1), word[i].unsqueeze(0))
-
-        # Backprop
-        loss.backward()
-        optimizer.step()
-
-        return loss.item()
-
-class Dataset():
+class CharacterDataset(Dataset):
 
     def __init__(self):
-        with open('data_small.txt', 'r') as content_file:
-            self.content = [list(word) for word in content_file.read().lower().strip('\n').replace('\n', ' ') if len(list(word)) > 0 ]
+        with open(DATASET, 'r') as content_file:
+            self.content = [list(word) for line in content_file for word in line.strip().lower()]
 
-    def training_batches(self):
+        dataset = []
+        self.characters = Characters()
         for word in self.content:
-            yield word
+            dataset.append(self.characters.add_char(word[0]))
+
+        self.dataset = torch.Tensor(dataset).long()
+
+    @property
+    def vocab_size(self):
+        return len(self.characters)
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+
+        data = self.dataset[index:index + SEQ_LEN]
+        return data
+
+    def __len__(self):
+        return len(self.content) - BATCH_SIZE
+
 
 class Trainer():
 
     def __init__(self, num_epochs):
+
         self.criterion = NLLLoss()
-        self.characters = Characters()
-        self.dataset = Dataset()
-        for data in self.dataset.training_batches():
-            self.characters.add_word(data)
-        self.model = Model(len(self.characters))
-        self.optimizer = Adam(self.model.parameters(), lr=0.01)
+        self.dataset = CharacterDataset()
+        dataloader_kwargs = {'num_workers': 1, 'pin_memory': True} if torch.cuda.is_available() else {}
+        self.dataloader = DataLoader(self.dataset, batch_size=BATCH_SIZE, shuffle=True, **dataloader_kwargs)
+
+        self.model = Model(self.dataset.vocab_size).to(device)
+        self.optimizer = Adam(self.model.parameters(), lr=LR)
+
         self.epoch = 0
         self.num_epochs = num_epochs
         self.total_loss = 0.0
 
     def train(self):
         for self.epoch in range(self.epoch + 1, self.num_epochs + 1):
-            for batch in self.dataset.training_batches():
-                batch = torch.tensor(self.characters.add_word(batch))
-                loss = \
-                    self.model.train(batch, self.optimizer, self.criterion)
-                self.total_loss += loss
-            print(self.total_loss)
             self.total_loss = 0.0
+            for batch_idx, batch in enumerate(self.dataloader):
+                batch = batch.to(device)
+                self.optimizer.zero_grad()
+
+                output = self.model(batch)
+                loss = self.criterion(output[:, :-1].reshape(-1, self.dataset.vocab_size), batch[:, 1:].reshape(-1))
+
+                loss.backward()
+                self.optimizer.step()
+                self.total_loss += loss.item()
+                if batch_idx % 10 == 0:
+                    print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    self.epoch, batch_idx * BATCH_SIZE, len(self.dataloader.dataset),
+                    100. * batch_idx / len(self.dataloader), loss.item()))
+
+            print('====> Train set loss: {:.4f}'.format(self.total_loss / len(self.dataloader)))
 
 
-
-t = Trainer(1000)
-# t.train()
-for i in t.dataset.training_batches():
-    print(i)
-
-
-
-
-
-
-
-
-
-
-
-
-#
+if __name__ == "__main__":
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    t = Trainer(EPOCHS)
+    t.train()
